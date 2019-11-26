530d85cd53c78b7e3dd5b1b1fc6eaf926768504b
wu-cl <44141526+wu-cl@users.noreply.github.com>
2019-11-26 18:39:13 +0800

    Update setting.sh

--- setting.sh
+++ setting.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 sudo apt-get install zsh
 chsh -s /bin/zsh
+sudo sed -i 's#root:/bin/bash#root:/bin/zsh#' /etc/passwd
 sudo apt-get install git
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
 echo "source ${(q-)PWD}/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> ${ZDOTDIR:-$HOME}/.zshrc
 git clone git://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
 echo "source $ZSH_CUSTOM/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh" >> ${ZDOTDIR:-$HOME}/.zshrc
+sed -i 's#ZSH_THEME=\"robbyrussell\"#ZSH_THEME=\"risto\"#' ~/.zshrc
 source ~/.zshrc
