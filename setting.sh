72501d563c03d857a67141f3d42e018e3e23b3d8
wu-cl <44141526+wu-cl@users.noreply.github.com>
2019-11-26 18:21:34 +0800

    Create setting.sh

---
+++ setting.sh
@@ -0,0 +1,10 @@
+#
+sudo apt-get install zsh
+chsh -s /bin/zsh
+sudo apt-get install git
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
+echo "source ${(q-)PWD}/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> ${ZDOTDIR:-$HOME}/.zshrc
+git clone git://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
+echo "source $ZSH_CUSTOM/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh" >> ${ZDOTDIR:-$HOME}/.zshrc
+source ~/.zshrc
