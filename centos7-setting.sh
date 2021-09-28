333294e5f5f9e049f4ea0d36abf9e16df0c4bd4d
wu-cl <44141526+wu-cl@users.noreply.github.com>
2021-09-28 14:08:36 +0800

    Update centos7-setting.sh

--- centos7-setting.sh
+++ centos7-setting.sh
@@ -24,3 +24,30 @@ yum -y --enablerepo=gf-plus install vim-enhanced sudo
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
 wget https://raw.githubusercontent.com/wu-cl/zsh_vim/master/.vimrc -O ~/.vimrc
+
+
+# tmux
+# install deps
+sudo yum install -y gcc kernel-devel make ncurses-devel
+
+# DOWNLOAD SOURCES FOR LIBEVENT AND MAKE AND INSTALL
+curl -LOk https://github.com/libevent/libevent/releases/download/release-2.1.11-stable/libevent-2.1.11-stable.tar.gz
+tar -xf libevent-2.1.11-stable.tar.gz
+cd libevent-2.1.11-stable
+./configure --prefix=/usr/local
+make
+sudo make install
+
+# DOWNLOAD SOURCES FOR TMUX AND MAKE AND INSTALL
+
+curl -LOk https://github.com/tmux/tmux/releases/download/3.2a/tmux-3.2a.tar.gz
+tar -xf tmux-3.2a.tar.gz
+cd tmux-3.2a
+LDFLAGS="-L/usr/local/lib -Wl,-rpath=/usr/local/lib" ./configure --prefix=/usr/local
+make
+sudo make install
+
+# pkill tmux
+# close your terminal window (flushes cached tmux executable)
+# open new shell and check tmux version
+tmux -V
